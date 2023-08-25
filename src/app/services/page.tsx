fe760732455c74b997d812b04799a4feb7567c06
swaraj Saxena <swarishraj00@gmail.com>
2023-08-25 23:21:47 +0530

    xyz

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -14,9 +14,9 @@ const page = () => {
   ]
   return (
     <div className='flex-1 grid grid-cols-1 place-items-center px-4 '>
-      <div className='p-[2px] max-w-[50%] relative group rounded-full overflow-hidden cursor-pointer'>
+      <div className='p-[2px] max-w-[50%] relative group rounded-[3rem] overflow-hidden cursor-pointer'>
         <div className='absolute top-0 left-1/2 -translate-x-1/2 w-[0px] transition-all group-hover:w-full h-full bg-gradient-to-r from-accent/50 via-accent/10 to-accent/50 opacity-0 group-hover:opacity-70'></div>
-        <div className='p-12 px-16 relative z-50 bg-mutedBlack pointer-events-none rounded-full transition-all flex flex-col justify-start gap-2'>
+        <div className='p-12 border border-white/5 px-16 relative z-50 bg-mutedBlack pointer-events-none rounded-[3rem] transition-all flex flex-col justify-start gap-2'>
           <div className='font-bold text-2xl flex items-center justify-between w-full'>
             Services
             <NextLink
