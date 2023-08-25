f5c0201d593c11c6dfd6e1ebf7380a95ac0be7d5
swaraj Saxena <swarishraj00@gmail.com>
2023-08-25 23:17:47 +0530

    design for service page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,4 +1,5 @@
 import Divider from '@/components/Divider'
+import NextLink from '@/components/NextLink'
 import Image from 'next/image'
 import React from 'react'
 
@@ -12,32 +13,27 @@ const page = () => {
     'Blog/News Website',
   ]
   return (
-    <div className='flex-1 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-4'>
-      {services.map((ser, index) => {
-        return (
-          <div className='relative flex flex-col border border-mutedWhite/10 justify-between rounded-lg overflow-hidden'>
-            <div className='relative z-10 p-4 bg-black/80 h-full flex flex-col justify-start items-stretch gap-2 backdrop-blur-sm'>
-              <div className='text-4xl font-bold normal-case '>{ser}</div>
-              <Divider />
-              <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Architecto aspernatur minus quod minima soluta explicabo nemo ex
-                nobis hic! Provident aut possimus molestias tempore! Tempora,
-                dolores maiores. Quidem ipsam expedita assumenda soluta
-                voluptate quia voluptatibus doloremque dicta ad, libero, quam
-                dolores nesciunt in fuga asperiores, debitis consequatur
-                molestiae aspernatur tempora.
-              </div>
-            </div>
-            <Image
-              src={'/upsiidedown.png'}
-              alt='upsiidedown'
-              fill
-              className='object-cover rotate-180 scale-110'
+    <div className='flex-1 grid grid-cols-1 place-items-center px-4 '>
+      <div className='p-[2px] max-w-[50%] relative group rounded-full overflow-hidden cursor-pointer'>
+        <div className='absolute top-0 left-1/2 -translate-x-1/2 w-[0px] transition-all group-hover:w-full h-full bg-gradient-to-r from-accent/50 via-accent/10 to-accent/50 opacity-0 group-hover:opacity-70'></div>
+        <div className='p-12 px-16 relative z-50 bg-mutedBlack pointer-events-none rounded-full transition-all flex flex-col justify-start gap-2'>
+          <div className='font-bold text-2xl flex items-center justify-between w-full'>
+            Services
+            <NextLink
+              label='View More'
+              path=''
             />
           </div>
-        )
-      })}
+          <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            explicabo ea, natus facilis dolorem harum! Quisquam illum voluptatem
+            praesentium mollitia ad provident corrupti sequi inventore alias
+            optio at, obcaecati quia, nesciunt, animi autem culpa commodi
+            voluptates. Aut quas assumenda in deserunt corporis, harum autem,
+            illum culpa esse hic nam sequi!
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
