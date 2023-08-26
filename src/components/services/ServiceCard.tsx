775f562282827bbfe36cdca52567803437d697b5
Sarthak Gupta <gsarthak783891@gmail.com>
2023-08-27 02:03:56 +0530

    service pageee

--- src/components/services/ServiceCard.tsx
+++ src/components/services/ServiceCard.tsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import NextLink from '../NextLink'
 import { ServicesI } from '@/app/services/page'
+import Divider from '@/components/Divider'
 
 const ServiceCard = ({ element }: { element: ServicesI }) => {
   return (
     // "flex-1 grid grid-cols-1 place-items-center"
     <div className=' px-4 '>
-      <div className='p-[2px]  relative group rounded-[50px] overflow-hidden cursor-pointer'>
-        <div className='absolute top-0 left-1/2 -translate-x-1/2 w-[0px] transition-all group-hover:w-full h-full bg-gradient-to-r from-accent/50 via-accent/10 to-accent/50 opacity-0 group-hover:opacity-70'></div>
-        <div className='p-12 border border-white/5 px-16 relative z-50 bg-mutedBlack pointer-events-none rounded-[3rem] transition-all flex flex-col justify-start gap-2'>
+      <div className='p-[2px]  relative group  overflow-hidden cursor-pointer hover:bg-white/90 hover:text-mutedBlack transition-all'>
+        <div className='absolute top-0 left-1/2 -translate-x-1/2 w-[0px] transition-all  h-full bg-gradient-to-r  opacity-0 '></div>
+        <div className='p-10  px-16 relative z-50  pointer-events-none  transition-all flex flex-col justify-between gap-2'>
           <div className='font-bold text-2xl flex items-center justify-between w-full'>
             {element.title}
-            <NextLink
-              label='View More'
-              path=''
-            />
           </div>
-          <div>{element.description}</div>
+          <div>
+            {element.description.substr(0, 250) + ' ... '}
+            <span className=' font-bold '>Read More</span>
+          </div>
         </div>
       </div>
+      {/* <Divider icons={false} /> */}
     </div>
   )
 }
