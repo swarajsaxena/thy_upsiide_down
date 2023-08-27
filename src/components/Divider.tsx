0dc36ba732af246a26250a8ca2e385e303e32fef
swaraj Saxena <swarishraj00@gmail.com>
2023-08-27 12:45:30 +0530

    tailwind merge added to divider

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -3,22 +3,27 @@
 import React from 'react'
 import { FiPlus } from 'react-icons/fi'
 import { motion } from 'framer-motion'
+import { twMerge } from 'tailwind-merge'
 
 const Divider = ({
   orientation = 'horizontal',
   icons = true,
+  customStyles = '',
 }: {
   orientation?: 'vertical' | 'horizontal'
   icons?: boolean
+  customStyles?: string
 }) => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 0.5 }}
-      className={`text-mutedWhite/50 text-2xl items-center flex gap-2 justify-between  ${
-        orientation === 'horizontal' ? 'flex-row' : 'flex-col hidden md:flex'
-      }`}
+      className={twMerge(
+        'text-mutedWhite/50 text-2xl items-center flex gap-2 justify-between',
+        orientation === 'horizontal' ? 'flex-row' : 'flex-col hidden md:flex',
+        customStyles
+      )}
     >
       {icons && <FiPlus className='text-accent' />}
       <div
