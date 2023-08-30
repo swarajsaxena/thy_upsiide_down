4d73f2fc3b28f0e16ff68b20b1430d4c50594ead
swaraj Saxena <swarishraj00@gmail.com>
2023-08-30 21:25:45 +0530

    font location changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,39 +8,39 @@ import localFont from 'next/font/local'
 
 const bauziet = localFont({
   src: [
-    { path: './fonts/BauzietNormlight.ttf', weight: '300' },
-    { path: './fonts/BauzietNormRegular.ttf', weight: '400' },
-    { path: './fonts/BauzietNormMedium.ttf', weight: '500' },
-    { path: './fonts/BauzietNormSemiBold.ttf', weight: '600' },
-    { path: './fonts/BauzietNormBold.ttf', weight: '700' },
-    { path: './fonts/BauzietNormExtraBold.ttf', weight: '800' },
+    { path: '../../public/fonts/BauzietNormLight.ttf', weight: '300' },
+    { path: '../../public/fonts/BauzietNormRegular.ttf', weight: '400' },
+    { path: '../../public/fonts/BauzietNormMedium.ttf', weight: '500' },
+    { path: '../../public/fonts/BauzietNormSemiBold.ttf', weight: '600' },
+    { path: '../../public/fonts/BauzietNormBold.ttf', weight: '700' },
+    { path: '../../public/fonts/BauzietNormExtraBold.ttf', weight: '800' },
     {
-      path: './fonts/BauzietNormlightItalic.ttf',
+      path: '../../public/fonts/BauzietNormlightItalic.ttf',
       weight: '300',
       style: 'italic',
     },
     {
-      path: './fonts/BauzietNormRegularItalic.ttf',
+      path: '../../public/fonts/BauzietNormRegularItalic.ttf',
       weight: '400',
       style: 'italic',
     },
     {
-      path: './fonts/BauzietNormMediumItalic.ttf',
+      path: '../../public/fonts/BauzietNormMediumItalic.ttf',
       weight: '500',
       style: 'italic',
     },
     {
-      path: './fonts/BauzietNormSemiBoldItalic.ttf',
+      path: '../../public/fonts/BauzietNormSemiBoldItalic.ttf',
       weight: '600',
       style: 'italic',
     },
     {
-      path: './fonts/BauzietNormBoldItalic.ttf',
+      path: '../../public/fonts/BauzietNormBoldItalic.ttf',
       weight: '700',
       style: 'italic',
     },
     {
-      path: './fonts/BauzietNormExtraBoldItalic.ttf',
+      path: '../../public/fonts/BauzietNormExtraBoldItalic.ttf',
       weight: '800',
       style: 'italic',
     },
