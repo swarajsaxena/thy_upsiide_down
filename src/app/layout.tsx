08201a644ba794b28be12ee16d3cb75f701fb294
swaraj Saxena <swarishraj00@gmail.com>
2023-08-30 21:18:54 +0530

    font file name changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,39 +8,39 @@ import localFont from 'next/font/local'
 
 const bauziet = localFont({
   src: [
-    { path: './fonts/Bauziet Norm light.ttf', weight: '300' },
-    { path: './fonts/Bauziet Norm Regular.ttf', weight: '400' },
-    { path: './fonts/Bauziet Norm Medium.ttf', weight: '500' },
-    { path: './fonts/Bauziet Norm SemiBold.ttf', weight: '600' },
-    { path: './fonts/Bauziet Norm Bold.ttf', weight: '700' },
-    { path: './fonts/Bauziet Norm ExtraBold.ttf', weight: '800' },
+    { path: './fonts/BauzietNormlight.ttf', weight: '300' },
+    { path: './fonts/BauzietNormRegular.ttf', weight: '400' },
+    { path: './fonts/BauzietNormMedium.ttf', weight: '500' },
+    { path: './fonts/BauzietNormSemiBold.ttf', weight: '600' },
+    { path: './fonts/BauzietNormBold.ttf', weight: '700' },
+    { path: './fonts/BauzietNormExtraBold.ttf', weight: '800' },
     {
-      path: './fonts/Bauziet Norm light Italic.ttf',
+      path: './fonts/BauzietNormlightItalic.ttf',
       weight: '300',
       style: 'italic',
     },
     {
-      path: './fonts/Bauziet Norm Regular Italic.ttf',
+      path: './fonts/BauzietNormRegularItalic.ttf',
       weight: '400',
       style: 'italic',
     },
     {
-      path: './fonts/Bauziet Norm Medium Italic.ttf',
+      path: './fonts/BauzietNormMediumItalic.ttf',
       weight: '500',
       style: 'italic',
     },
     {
-      path: './fonts/Bauziet Norm SemiBold Italic.ttf',
+      path: './fonts/BauzietNormSemiBoldItalic.ttf',
       weight: '600',
       style: 'italic',
     },
     {
-      path: './fonts/Bauziet Norm Bold Italic.ttf',
+      path: './fonts/BauzietNormBoldItalic.ttf',
       weight: '700',
       style: 'italic',
     },
     {
-      path: './fonts/Bauziet Norm ExtraBold Italic.ttf',
+      path: './fonts/BauzietNormExtraBoldItalic.ttf',
       weight: '800',
       style: 'italic',
     },
