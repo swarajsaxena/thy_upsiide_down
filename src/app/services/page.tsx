667e2cef08b45f31f7667e77f5d89c2d43c44b37
Sarthak Gupta <gsarthak783891@gmail.com>
2023-08-30 16:13:26 +0530

    hello

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -11,12 +11,7 @@ const page = () => {
   const [activeService, setActiveService] = useState<ServicesI>(services[0])
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 1 }}
-      className='mx-4 mb-0 md:flex-grow flex md:overflow-y-hidden overflow-scroll scrollbar-none flex-col-reverse md:flex-row'
-    >
+    <motion.div className='mx-4 mb-0 md:flex-grow flex md:overflow-y-hidden overflow-scroll scrollbar-none flex-col-reverse md:flex-row'>
       <WhyChooseUs />
       <Divider
         icons={false}
